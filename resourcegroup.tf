4fc96729f77dd40c499c4c241c87e418790fbac6
peronealex <75041191+peronealex@users.noreply.github.com>
2020-12-09 11:05:12 +0000

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resourceaz" {
   name     = var.name
   location = var.location
-  //tags = 
+  tags =  merge({"Name" = format("%s", var.resource_group_name)}, var.tags,)
 }
